42732d3b6f7dd43d74acfe27445c1ad5ba2ebf55
Mohammed Aslam M K <117348902+iam-Aslam@users.noreply.github.com>
2023-08-03 13:54:53 +0530

    Date changed to dynamic

--- lib/controllers/repo_provider.dart
+++ lib/controllers/repo_provider.dart
@@ -2,14 +2,26 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:githubapi/models/respository_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class RepositoryProviderModel extends ChangeNotifier {
   List<RepositoryModel> repositories = [];
 
   //this methos is used to get the data from the api and it will emit data as the created model(RepositoryModel)
   Future<void> fetchData() async {
+    //this method will get the exact datas before 60 days
+    String getDateTwoMonthsAgo() {
+      var now = DateTime.now();
+      var twoMonthsAgo =
+          now.subtract(const Duration(days: 60)); // 60 days = 2 months
+      var formatter = DateFormat('yyyy-MM-dd');
+      return formatter.format(twoMonthsAgo);
+    }
+
+    String date = getDateTwoMonthsAgo();
+
     final response = await http.get(Uri.parse(
-        'https://api.github.com/search/repositories?q=created:>2022-04-29&sort=stars&order=desc'));
+        'https://api.github.com/search/repositories?q=created:>$date&sort=stars&order=desc'));
 
     if (response.statusCode == 200) {
       final jsonData = jsonDecode(response.body);
