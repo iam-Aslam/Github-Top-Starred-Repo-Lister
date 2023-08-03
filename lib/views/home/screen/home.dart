68f07e89176d6afca667d42dbad294779ed28061
Mohammed Aslam M K <117348902+iam-Aslam@users.noreply.github.com>
2023-08-03 16:32:50 +0530

    Refreshing added

--- lib/views/home/screen/home.dart
+++ lib/views/home/screen/home.dart
@@ -18,34 +18,42 @@ class Home extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 10.0),
-          child: Consumer<RepositoryProviderModel>(
-            builder: (context, provider, _) {
-              if (provider.repositories.isEmpty) {
-                provider.fetchData();
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                return ListView.separated(
-                  itemCount: provider.repositories.length,
-                  separatorBuilder: (context, index) {
-                    return SizedBox(
-                      height: MediaQuery.of(context).size.height / 60,
-                    );
-                  },
-                  itemBuilder: (context, index) {
-                    final repository = provider.repositories[index];
-                    return ListCard(
-                      title: repository.title,
-                      description: repository.description,
-                      imageUrl: repository.imageUrl,
-                      user: repository.user,
-                      starCount: repository.starCount,
-                    );
-                  },
-                );
-              }
-            },
+        body: RefreshIndicator(
+          onRefresh: () async {
+            // will Refresh the data
+            final provider =
+                Provider.of<RepositoryProviderModel>(context, listen: false);
+            await provider.fetchData();
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Consumer<RepositoryProviderModel>(
+              builder: (context, provider, _) {
+                if (provider.repositories.isEmpty) {
+                  provider.fetchData();
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return ListView.separated(
+                    itemCount: provider.repositories.length,
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        height: MediaQuery.of(context).size.height / 60,
+                      );
+                    },
+                    itemBuilder: (context, index) {
+                      final repository = provider.repositories[index];
+                      return ListCard(
+                        title: repository.title,
+                        description: repository.description,
+                        imageUrl: repository.imageUrl,
+                        user: repository.user,
+                        starCount: repository.starCount,
+                      );
+                    },
+                  );
+                }
+              },
+            ),
           ),
         ),
       ),
